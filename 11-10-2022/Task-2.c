69ce7221041aab0d7893f427d8c1ee70191dc45a
Arman Mokammel <armanmokammel@gmail.com>
2022-10-11 20:44:55 +0600

    Create Task-2.c

---
+++ 11-10-2022/Task-2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    int A,B,C;
+    float Avg;
+
+    printf("Insert first number: ");
+    scanf("%d", &A);
+
+    printf("Insert second number: ");
+    scanf("%d", &B);
+
+    printf("Insert third number: ");
+    scanf("%d", &C);
+
+    Avg = (A + B + C) / 3.0; //We use 3.0 instead of 3 so that the output becomes a float instead of int
+
+    printf("The average is: %.1f", Avg); //The ".1" specifies the number decimal places to be shown which in our case is 1 decimal place
+
+    return 0;
+}
