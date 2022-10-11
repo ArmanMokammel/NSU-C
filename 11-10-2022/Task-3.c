b52f479c0e1504d97c16177ea3f4d40e0db413bd
Arman Mokammel <armanmokammel@gmail.com>
2022-10-11 21:22:47 +0600

    Update Task-3.c

--- 11-10-2022/Task-3.c
+++ 11-10-2022/Task-3.c
@@ -6,7 +6,7 @@ int main()
     printf("Enter Celsius: ");
     scanf("%f", &C);
 
-    float F = (C * (9.0 / 5.0)) + 32.0; //The formula F=((C*9)/5)+32 . We use floats in the equation.
+    float F = (C * (9.0 / 5.0)) + 32.0; //The formula F=(C*(9/5)+32 . We use floats in the equation.
 
     printf("Fahrenheit value is = %.2f", F); //Correct to 2 decimal places
     /* The number of decimal places to be shown will be given */
