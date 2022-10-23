409a538e3bd8eee80e8df23241d4cc961d19bc1d
Arman Mokammel <armanmokammel@gmail.com>
2022-10-23 17:48:13 +0600

    Create Ex-Task-4.c

---
+++ 23-10-2022/Ex-Task-4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if((num % 5 == 0) && (num % 11 == 0))
+    {
+        printf("%d is a multiple of both 5 and 11", num);
+    }
+    else if(num % 5 == 0)
+    {
+        printf("%d is a multiple of 5", num);
+    }
+    else if (num % 11 == 0)
+    {
+        printf("%d is a multiple 11", num);
+    }
+
+    return 0;
+}
