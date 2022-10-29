1df68a4166ce1c83fc933f4b52fdb3595b1b0d93
Arman Mokammel <armanmokammel@gmail.com>
2022-10-29 22:48:06 +0600

    Create HW-Task-1-(B).c

---
+++ 18-10-2022/HW-Task-1-(B).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    float b,h;
+    float area;
+    
+    printf("Enter the base of Parallelogram: ");
+    scanf("%f", &b);
+    printf("Enter the base of Parallelogram: ");
+    scanf("%f", &h);
+  
+    area = b * h;
+    printf("Area of Parallelogram= %.2f",area);
+
+    return 0;
+}
