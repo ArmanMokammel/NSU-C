94abc8c0807c676bbbf3bef421014aba08b1fbb8
Arman Mokammel <armanmokammel@gmail.com>
2022-10-29 22:31:22 +0600

    Update Ex-Task-1.c

--- 18-10-2022/Ex-Task-1.c
+++ 18-10-2022/Ex-Task-1.c
@@ -2,13 +2,13 @@
 
 int main()
 {
-  int n;
-  printf("Enter the number: ");
-  scanf("%d",&n);
-  printf("%d",n&1);
+    int n;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    printf("%d",n&1);
   
-  return 0;
+    return 0;
   
-  /* The obervation is: For even numbers, n&1=0 and for odd numbers n&1 = 1 */
+    /* The obervation is: For even numbers, n&1=0 and for odd numbers n&1 = 1 */
   
 }
