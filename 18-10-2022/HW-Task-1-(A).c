ac5f38ad1f860faad0b395f02a9310f447c97e25
Arman Mokammel <armanmokammel@gmail.com>
2022-10-29 22:45:00 +0600

    Create HW-Task-1-(A).c

---
+++ 18-10-2022/HW-Task-1-(A).c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    float b1,b2,h;
+    
+    printf("Enter first base of Trapezium: ");
+    scanf("%f", &b1);
+  
+    printf("Enter second base of Trapezium: ");
+    scanf("%f", &b2);
+  
+    printf("Enter height base of Trapezium: ");
+    scanf("%f", &h);
+    
+    float area = ((b1 + b2) / 2) * h;
+    printf("Area of Trapezium = %.2f",area);
+  
+    return 0;
+}
