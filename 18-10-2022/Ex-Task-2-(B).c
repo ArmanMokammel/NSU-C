d6e280d486ab2fa28d497d5f04be3c661ad0e701
Arman Mokammel <armanmokammel@gmail.com>
2022-10-29 22:32:35 +0600

    Update Ex-Task-2-(B).c

--- 18-10-2022/Ex-Task-2-(B).c
+++ 18-10-2022/Ex-Task-2-(B).c
@@ -3,16 +3,16 @@
 
 int main()
 {
-  int m,n;
+    int m,n;
   
-  printf("Enter the numbers: ");
-  scanf("%d%d",&n,&m);
+    printf("Enter the numbers: ");
+    scanf("%d%d",&n,&m);
   
-  printf("%.0f\n",m/pow(2,n)); // m / 2^n
-  printf("%d",m>>n);
+    printf("%.0f\n",m/pow(2,n)); // m / 2^n
+    printf("%d",m>>n);
   
-  return 0;
+    return 0;
   
-  /* The observation is: Both values are equivalent */
+    /* The observation is: Both values are equivalent */
   
 }
