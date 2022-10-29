a889293220467393ed9fceca30f8196c288820b2
Arman Mokammel <armanmokammel@gmail.com>
2022-10-29 22:23:49 +0600

    Create Ex-Task-2.c

---
+++ 18-10-2022/Ex-Task-2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+  int m,n;
+  
+  printf("Enter the numbers: ");
+  scanf("%d%d",&n,&m);
+  
+  printf("%.0f\n", m*pow(2,n)); // m * 2^n
+  printf("%d",m<<n);
+  
+  return 0;
+  
+  /* The observation is: Both the values are equivalent */
+  
+}
