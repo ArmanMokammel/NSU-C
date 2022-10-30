e5a9a476cbe3bcb5d0f50941a11d66e66c9a2cfd
Arman Mokammel <armanmokammel@gmail.com>
2022-10-30 17:31:14 +0600

    Create Task-2.c

---
+++ LB-Quiz/LB-Quiz-1/Task-2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float x1, y1, r1;
+    float x2, y2, r2;
+    float x;
+
+    printf("Enter the x- , y-coordinate and radius of circle1: ");
+    scanf("%f%f%f", &x1, &y1, &r1);
+    printf("Enter the x- , y-coordinate and radius of circle2: ");
+    scanf(" %f%f%f", &x2, &y2, &r2);
+
+    float distance = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
+
+    if(r1 - r2 < 0)
+    {
+        x = -(r1 - r2);
+    }
+
+    if(distance <= x)
+    {
+        printf("circle2 is overlaping circle1");
+    }
+    else if(distance <= r1 + r2)
+    {
+        printf("circle2 is inside circle1");
+    }
+    else
+    {
+        printf("circle2 does not overlap circle1");
+    }
+
+    return 0;
+}
