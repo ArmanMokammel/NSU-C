856ae7fbedaecb29b7a8834ed8f06923632308f0
Arman Mokammel <armanmokammel@gmail.com>
2022-11-03 21:03:25 +0600

    Update Assign-Task-1.c

--- Lab-06/Assign-Task-1.c
+++ Lab-06/Assign-Task-1.c
@@ -8,10 +8,19 @@ int main()
     printf("Enter value of n: ");
     scanf("%d", &n);
     
+    while(i <= n)
+    {
+        sum += i*i;
+        i += j;
+        j += 2;
+    }
+    
+    /*
     for(i = 5, j = 4; i <= n; i+=j, j+=2)
     {
         sum += i*i;
     }
+    */
     
     printf("Sum = %0.0f", sum);
     
