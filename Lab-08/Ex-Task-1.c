e37582e419b5381875ed7f019f042a64c39777b1
Arman Mokammel <armanmokammel@gmail.com>
2022-11-12 03:58:05 +0600

    Update Ex-Task-1.c

--- Lab-08/Ex-Task-1.c
+++ Lab-08/Ex-Task-1.c
@@ -7,13 +7,13 @@ int main()
     printf("Enter two numbers: ");
     scanf("%d%d", &num1, &num2);
 
-    min = num1 < num2 ? num1 : num2;
+    min = num1 < num2 ? num1 : num2; // Taking the lowest input value
 
-    for(i = min; i >= 1; i--)
+    for(i = min; i >= 1; i--) // We start with the lowest input value and decrement by one... #efficient_code
     {
         if(num1 % i == 0 && num2 % i == 0)
         {
-            gcd = i;
+            gcd = i; // Found goddammit
             break;
         }
     }
