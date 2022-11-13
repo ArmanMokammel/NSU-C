aca75b419a553fb824d8559c8ff5e527f211acae
Arman Mokammel <armanmokammel@gmail.com>
2022-11-13 22:55:55 +0600

    Update Assign-Task-1.c

--- Lab-09/Assign-Task-1.c
+++ Lab-09/Assign-Task-1.c
@@ -7,15 +7,22 @@ int main()
     printf("Enter N: ");
     scanf("%d", &N);
 
+    // for each line i do the following loop
     for(i = 1; i <= N; i++)
     {
+        /* Calculating the spaces first 
+           For each character j do the following loop */
         for(j = 1; j <= N - i; j++)
         {
+            // print space
             printf(" ");
         }
-
+        
+        // After adding appropriate spaces check for 2 conditions
+        // If i == 1 that is if it is first line or if i == N that is if it is last line
         if(i == 1 || i == N)
         {
+            // print N number of characters
             for(j = 1; j <= N; j++)
             {
                 printf("*");
@@ -23,14 +30,18 @@ int main()
         }
         else
         {
+            /* Otherwise for each character j of line i do the following loop */
             for(j=1; j<=N; j++)
             {
+                // Check for 2 conditions
+                // If j == 1 that is if it is first character or if j == N that is if it is last character
                 if(j == 1 || j == N)
                 {
                     printf("*");
                 }
                 else
                 {
+                    // print space
                     printf(" ");
                 }
             }
