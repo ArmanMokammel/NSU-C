db2823e79384867036c84d17c734ecf60195ae60
Arman Mokammel <armanmokammel@gmail.com>
2022-11-13 23:05:53 +0600

    Update Assign-Task-2.c

--- Lab-09/Assign-Task-2.c
+++ Lab-09/Assign-Task-2.c
@@ -6,26 +6,34 @@ int main()
 
     printf("Enter N: ");
     scanf("%d", &N);
-
+    
+    /* for each line i do the following loop */
     for(i=1; i<=N; i++)
     {
+        /* Calculating the spaces first 
+           For each character j do the following loop */
         for(j = 1; j <= N - i; j++)
         {
+            // print spaces
             printf(" ");
         }
-
+        
+        /* For each character j of line i do the following loop */
         for(j=1; j<=i; j++)
         {
+            // Check for 3 conditions
+            // If j == 1 that is if it is first character or if j == i that is if it is the last character of if i == n that is if it is the last line
             if(j==1 || j==i || i==N)
             {
                 printf("*");
             }
             else
             {
+                // print spaces
                 printf(" ");
             }
-        }
-
+        }        
+        //print a new line character at the end of each line (loop of i) for the display to go to the next line below it
         printf("\n");
     }
 
