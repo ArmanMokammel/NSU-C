a1e12ec4db47dd60e7f49c4d21a29a57ec9fe73a
Arman Mokammel <armanmokammel@gmail.com>
2022-11-17 19:43:04 +0600

    Update Ex-Task-1-03.c

--- Lab-10/Ex-Task-1-03.c
+++ Lab-10/Ex-Task-1-03.c
@@ -4,22 +4,32 @@ int main()
 {
     int rows, i, j;
     char ch;
-
+    
     printf("Enter the number of rows: ");
     scanf("%d", &rows);
-
-    for(int i = 1; i <= rows; i++)
+    
+    /* for each line i do the following loop */
+    for(i = 1; i <= rows; i++)
     {
-        for(int j = rows - 1; j >= i; j--)
+        /* Calculating the spaces first
+           For each character j do the following loop */
+        for(j = rows - 1; j >= i; j--)
         {
+            // print space
             printf(" ");
         }
-
+        
+        /* 
+           Let's take A as 65 & i as 2
+           The loop's condition will be from 65 to ((2 * 2) - 1) i.e. 68
+           This means the loop will run 3 times and will print 3 characters A, B & C
+        */
         for(ch = 'A'; ch < 'A' + (2*i) - 1; ch++)
         {
             printf("%c", ch);
         }
-
+        
+        //print a new line character at the end of each line (loop of i) for the display to go to the next line below it
         printf("\n");
     }
     return 0;
