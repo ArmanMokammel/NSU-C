7b461e5a043f94d8cfec5c9cd13bb02e73399b93
Arman Mokammel <armanmokammel@gmail.com>
2022-11-17 20:20:18 +0600

    Create Assign-Task-1-01.c

---
+++ Lab-10/Assign-Task-1-01.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, N;
+
+    printf("Enter N: ");
+    scanf("%d", &N);
+
+    /* 
+       for each line i do the following loop 
+       We start by printing the last line and at the end print the first line
+    */
+    for(i=N; i >= 1; i--)
+    {
+        /* Calculating the spaces first
+           For each character j do the following loop */
+        for(j = 1; j <= N - i; j++)
+        {
+            // print spaces
+            printf(" ");
+        }
+
+        /* For each character j of line i do the following loop */
+        for(j=1; j<=i; j++)
+        {
+            // Check for 3 conditions
+            // If j == 1 that is if it is first character or if j == i that is if it is the last character of if i == n that is if it is the last line
+            if(j==1 || j==i || i==N)
+            {
+                printf("*");
+            }
+            else
+            {
+                // print spaces
+                printf(" ");
+            }
+        }
+        //print a new line character at the end of each line (loop of i) for the display to go to the next line below it
+        printf("\n");
+    }
+
+    return 0;
+}
