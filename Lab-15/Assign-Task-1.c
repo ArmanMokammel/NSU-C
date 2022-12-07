6944b50505ca5e6119e6fbd30035910a7e188d7c
Arman Mokammel <armanmokammel@gmail.com>
2022-12-08 04:08:37 +0600

    Create Assign-Task-1.c

---
+++ Lab-15/Assign-Task-1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+  	char str[100];
+
+  	printf("Please Enter Any String : ");
+  	scanf("%[^\n]", &str);
+
+  	for(int i = 0; str[i] != NULL; i++)
+  	{
+        for(int j = i + 1; str[j] != NULL; j++)
+  		  {
+  			    if(str[j] == str[i])
+			      {
+  				      for(int k = j; str[k] != NULL; k++)
+				        {
+					          str[k] = str[k + 1];
+				        }
+ 			      }
+		    }
+	  }
+
+    printf("\nFinal String after Removing All Duplicates :\n%s\n", str);
+
+    return 0;
+}
