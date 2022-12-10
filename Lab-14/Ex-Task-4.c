4c0f897af32bf9f6c66061ac0d10e3af6c9860e7
Arman Mokammel <armanmokammel@gmail.com>
2022-12-10 21:19:47 +0600

    Update Ex-Task-4.c

--- Lab-14/Ex-Task-4.c
+++ Lab-14/Ex-Task-4.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
 
+int main()
+{
+    int size, INT_MIN = 0;
+
+    printf("Enter size of array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        printf("Enter [%d] = ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    INT_MIN = arr[0];
+
+    for(int i = 0; i < size; i++)
+    {
+        int x = arr[i];
+        if(INT_MIN > x)
+        {
+            INT_MIN = x;
+        }
+    }
+
+    printf("\nIndexes of smallest value: ");
+    for(int i = 0; i < size; i++)
+    {
+        if(arr[i] == INT_MIN)
+        {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
