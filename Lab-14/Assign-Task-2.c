82e00740173889ad51fc23566e66d3c71c13ff87
Arman Mokammel <armanmokammel@gmail.com>
2022-12-10 21:44:16 +0600

    Create Assign-Task-2.c

---
+++ Lab-14/Assign-Task-2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    int size, search_Key;
+
+    printf("Enter size of array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        printf("Enter [%d] = ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter search key: ");
+    scanf("%d", &search_Key);
+
+    for(int i = 0; i < size; i++)
+    {
+        if(arr[i] == search_Key)
+        {
+            for(int j = i; j < size - 1; j++)
+            {
+                arr[j] = arr[j + 1];
+            }
+            size--;
+        }
+    }
+
+    for(int i = 0; i < size; i++)
+    {
+        printf("\nIndex[%d] = %d", i, arr[i]);
+    }
+
+    return 0;
+}
