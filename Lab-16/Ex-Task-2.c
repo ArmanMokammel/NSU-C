16bd462c325b0f7bc704ee5623e1de46f69a271d
Arman Mokammel <armanmokammel@gmail.com>
2022-12-19 22:40:52 +0600

    Create Ex-Task-2.c

---
+++ Lab-16/Ex-Task-2.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int rows, cols;
+    printf("Enter row: ");
+    scanf("%d", &rows);
+    printf("Enter column: ");
+    scanf("%d", &cols);
+
+    int arr[rows][cols];
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j=0;j<cols;j++)
+        {
+            printf("Arr[%d][%d] = ", i, j);
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    printf("\n");
+
+    int sum=0;
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < cols; j++)
+        {
+            if(i==0 ||i==rows-1 || j==0 || j==cols-1)
+            {
+                sum=sum+arr[i][j];
+            }
+        }
+    }
+    printf("Sum=%d",sum);
+}
